3ef6961a410af131589587c2c622adf633f816b7
Tal Ben Hamo <tal6678@gmail.com>
2026-08-07 19:58:03 +0300

    Fix Flask startup during tests

--- app/app.py
+++ app/app.py
@@ -59,8 +59,7 @@ def health():
 
 
 if __name__ == "__main__":
-    # יצירת הטבלאות בבסיס הנתונים במידה והן לא קיימות
     with app.app_context():
         db.create_all()
 
-app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
